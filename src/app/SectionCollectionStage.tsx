741a10faf51881c789e1d870bf41597610266dd8
koo-2dub <71740212+koo-2dub@users.noreply.github.com>
2026-06-22 12:22:47 +0900

    Adjust Section 3 conveyor timing and stage size

--- src/app/SectionCollectionStage.tsx
+++ src/app/SectionCollectionStage.tsx
@@ -5,22 +5,24 @@ import Image from "next/image";
 
 const conveyorBoxes = [
   { src: "/assets/all-language/en-box.png", delay: "0s", rotate: "-3deg", width: 290, height: 320 },
-  { src: "/assets/all-language/jp-box.png", delay: "2.5s", rotate: "2deg", width: 288, height: 348 },
-  { src: "/assets/all-language/de-box.png", delay: "5s", rotate: "-1deg", width: 323, height: 348 },
-  { src: "/assets/all-language/fr-box.png", delay: "7.5s", rotate: "3deg", width: 263, height: 346 },
-  { src: "/assets/all-language/es-box.png", delay: "10s", rotate: "-2deg", width: 254, height: 326 },
-  { src: "/assets/all-language/ru-box.png", delay: "12.5s", rotate: "2deg", width: 276, height: 310 },
-  { src: "/assets/all-language/vn-box.png", delay: "15s", rotate: "-3deg", width: 312, height: 337 },
-  { src: "/assets/all-language/ar-box.png", delay: "17.5s", rotate: "1deg", width: 341, height: 358 },
-  { src: "/assets/all-language/po-box.png", delay: "20s", rotate: "-1deg", width: 333, height: 341 },
-  { src: "/assets/all-language/ch-box.png", delay: "22.5s", rotate: "3deg", width: 253, height: 321 },
-  { src: "/assets/all-language/in-box.png", delay: "25s", rotate: "-2deg", width: 260, height: 300 },
+  { src: "/assets/all-language/en-box.png", delay: "0s", rotate: "-3deg", width: 290, height: 320 },
+  { src: "/assets/all-language/jp-box.png", delay: "1.5s", rotate: "2deg", width: 288, height: 348 },
+  { src: "/assets/all-language/de-box.png", delay: "3s", rotate: "-1deg", width: 323, height: 348 },
+  { src: "/assets/all-language/fr-box.png", delay: "4.5s", rotate: "3deg", width: 263, height: 346 },
+  { src: "/assets/all-language/es-box.png", delay: "6s", rotate: "-2deg", width: 254, height: 326 },
+  { src: "/assets/all-language/ru-box.png", delay: "7.5s", rotate: "2deg", width: 276, height: 310 },
+  { src: "/assets/all-language/vn-box.png", delay: "9s", rotate: "-3deg", width: 312, height: 337 },
+  { src: "/assets/all-language/ar-box.png", delay: "10.5s", rotate: "1deg", width: 341, height: 358 },
+  { src: "/assets/all-language/po-box.png", delay: "12s", rotate: "-1deg", width: 333, height: 341 },
+  { src: "/assets/all-language/ch-box.png", delay: "13.5s", rotate: "3deg", width: 253, height: 321 },
+  { src: "/assets/all-language/in-box.png", delay: "15s", rotate: "-2deg", width: 260, height: 300 },
+
 ];
 
 export default function SectionCollectionStage() {
   return (
-    <div className="section-collection-stage relative mx-auto h-[760px] w-full max-w-[900px]">
-      <div className="section-collection-boxes absolute inset-x-0 top-0 z-10 mx-auto h-[590px] w-full">
+    <div className="section-collection-stage relative mx-auto">
+      <div className="section-collection-boxes absolute inset-x-0 top-0 z-10 mx-auto">
         {conveyorBoxes.map((box, index) => (
           <Image
             key={`language-box-${index}`}
@@ -44,7 +46,7 @@ export default function SectionCollectionStage() {
         alt="미니학습지 로고가 있는 컬렉션 베이스 박스"
         width={620}
         height={259}
-        className="section-collection-bottom-box absolute bottom-0 left-1/2 z-20 h-auto w-[620px] max-w-[92vw] -translate-x-1/2"
+        className="section-collection-bottom-box absolute bottom-0 left-1/2 z-20 h-auto -translate-x-1/2"
       />
     </div>
   );
